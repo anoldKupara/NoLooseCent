48a71f4ba179c4d525ae24f3c1395994d0fa9e26
Anold Kupara <kuparaanold20@gmail.com>
2025-05-29 19:12:30 +0200

    added the RecentTransactionViewModel class

---
+++ NoLooseCent/ViewModels/RecentTransactionViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NoLooseCent.ViewModels
+{
+    public class RecentTransactionViewModel
+    {
+        public string Type { get; set; }           // "Income" or "Expense"
+        public decimal Amount { get; set; }
+        public string SourceOrPurpose { get; set; }
+        public DateTime Date { get; set; }
+        public string CurrencyName { get; set; }
+    }
+}
