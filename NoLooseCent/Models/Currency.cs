2da0a3fb7fce596e275104850069f9fbe9201767
Anold Kupara <kuparaanold20@gmail.com>
2025-05-29 14:42:23 +0200

    added Currency model

---
+++ NoLooseCent/Models/Currency.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NoLooseCent.Models
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } // e.g., "USD Cash", "USD EcoCash"
+
+        [Required]
+        public string Code { get; set; } // e.g., "USD-CASH", "USD-ECO"
+
+        // Navigation Properties
+        public ICollection<Income> Incomes { get; set; }
+        public ICollection<Expense> Expenses { get; set; }
+    }
+}
