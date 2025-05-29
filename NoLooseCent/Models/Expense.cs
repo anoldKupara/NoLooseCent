5f86800e8b799b5b973b6c43d7328b86d58ba298
Anold Kupara <kuparaanold20@gmail.com>
2025-05-29 14:45:48 +0200

    added Expense model

--- NoLooseCent/Models/Expense.cs
+++ NoLooseCent/Models/Expense.cs
@@ -1,6 +1,23 @@
-﻿namespace NoLooseCent.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NoLooseCent.Models
 {
     public class Expense
     {
+        public int Id { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than zero")]
+        public decimal Amount { get; set; }
+
+        public string Purpose { get; set; } // e.g., "Groceries", "Rent"
+
+        [Required]
+        public DateTime DateSpent { get; set; }
+
+        // Foreign Key
+        [Required]
+        public int CurrencyId { get; set; }
+        public Currency Currency { get; set; }
     }
 }
