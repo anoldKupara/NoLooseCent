9b09e7b272b0866a4a48a05d027aee46b1491ad4
Anold Kupara <kuparaanold20@gmail.com>
2025-05-29 15:14:05 +0200

    added the CurrencyController

---
+++ NoLooseCent/Controllers/CurrencyController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NoLooseCent.DbContexts;
+using NoLooseCent.Models;
+
+namespace NoLooseCent.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CurrencyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Currencies.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var currency = await _context.Currencies.FindAsync(id);
+            if (currency == null) return NotFound();
+            return View(currency);
+        }
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Currency currency)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(currency);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(currency);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var currency = await _context.Currencies.FindAsync(id);
+            if (currency == null) return NotFound();
+            return View(currency);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Currency currency)
+        {
+            if (id != currency.Id) return NotFound();
+            if (ModelState.IsValid)
+            {
+                _context.Update(currency);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(currency);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var currency = await _context.Currencies.FindAsync(id);
+            if (currency == null) return NotFound();
+            return View(currency);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var currency = await _context.Currencies.FindAsync(id);
+            _context.Currencies.Remove(currency);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
