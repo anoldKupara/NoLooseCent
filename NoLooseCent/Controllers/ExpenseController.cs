3bc37dc105bf0dae61efdace897f0e0f21a32234
Anold Kupara <kuparaanold20@gmail.com>
2025-05-29 15:37:46 +0200

    added the ExpenseController

---
+++ NoLooseCent/Controllers/ExpenseController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NoLooseCent.DbContexts;
+using NoLooseCent.Models;
+
+namespace NoLooseCent.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExpenseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Expense
+        public async Task<IActionResult> Index()
+        {
+            var expenses = _context.Expenses.Include(e => e.Currency);
+            return View(await expenses.ToListAsync());
+        }
+
+        // GET: Expense/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var expense = await _context.Expenses.Include(e => e.Currency).FirstOrDefaultAsync(e => e.Id == id);
+            if (expense == null) return NotFound();
+            return View(expense);
+        }
+
+        // GET: Expense/Create
+        public IActionResult Create()
+        {
+            ViewBag.Currencies = _context.Currencies.ToList();
+            return View();
+        }
+
+        // POST: Expense/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(expense);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Currencies = _context.Currencies.ToList();
+            return View(expense);
+        }
+
+        // GET: Expense/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var expense = await _context.Expenses.FindAsync(id);
+            if (expense == null) return NotFound();
+            ViewBag.Currencies = _context.Currencies.ToList();
+            return View(expense);
+        }
+
+        // POST: Expense/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Expense expense)
+        {
+            if (id != expense.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(expense);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Currencies = _context.Currencies.ToList();
+            return View(expense);
+        }
+
+        // GET: Expense/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var expense = await _context.Expenses.Include(e => e.Currency).FirstOrDefaultAsync(e => e.Id == id);
+            if (expense == null) return NotFound();
+            return View(expense);
+        }
+
+        // POST: Expense/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var expense = await _context.Expenses.FindAsync(id);
+            _context.Expenses.Remove(expense);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
