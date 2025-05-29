4f5de9e8be14ff208059219b70adf13de9083609
Anold Kupara <kuparaanold20@gmail.com>
2025-05-29 14:40:10 +0200

    Add project files.

---
+++ NoLooseCent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NoLooseCent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
