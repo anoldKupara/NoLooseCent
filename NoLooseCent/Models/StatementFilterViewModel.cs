291613600260e1f759d448cc14cedaed701fe8be
Anold Kupara <kuparaanold20@gmail.com>
2025-07-01 19:17:49 +0200

    added StatementFilterViewModel to allow filtering of statements

---
+++ NoLooseCent/Models/StatementFilterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace NoLooseCent.Models
+{
+    public class StatementFilterViewModel
+    {
+        public int SelectedCurrencyId { get; set; }
+        public int SelectedYear { get; set; }
+        public int SelectedMonth { get; set; }
+
+        public IEnumerable<SelectListItem> Currencies { get; set; } = [];
+        public IEnumerable<SelectListItem> Months { get; set; } = [];
+    }
+}
