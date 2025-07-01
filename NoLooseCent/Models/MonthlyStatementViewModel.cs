534c3490a2d3479db5cfb15f8dae27a41c8e622c
Anold Kupara <kuparaanold20@gmail.com>
2025-07-01 19:08:57 +0200

    added the FinancialStatement view

--- NoLooseCent/Models/MonthlyStatementViewModel.cs
+++ NoLooseCent/Models/MonthlyStatementViewModel.cs
@@ -6,7 +6,7 @@
         public int Month { get; set; }
         public string MonthName => new DateTime(Year, Month, 1).ToString("MMMM yyyy");
 
-        public string CurrencyCode { get; set; }
+        public string CurrencyCode { get; set; } = "";
 
         public decimal OpeningBalance { get; set; }
         public decimal ClosingBalance { get; set; }
