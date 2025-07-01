59363ece24a1673f7a08e9c8976399317423ba42
Anold Kupara <kuparaanold20@gmail.com>
2025-07-01 19:19:21 +0200

    added FinancialStatementPageViewModel

---
+++ NoLooseCent/Reports/FinancialStatementPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using NoLooseCent.Models;
+
+namespace NoLooseCent.Reports
+{
+    public class FinancialStatementPageViewModel
+    {
+        public StatementFilterViewModel Filter { get; set; } = new();
+        public MonthlyStatementViewModel? Statement { get; set; }
+    }
+}
