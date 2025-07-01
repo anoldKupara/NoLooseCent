d8fc1e80a412dd70ef053fadfa4286794a184b28
Anold Kupara <kuparaanold20@gmail.com>
2025-07-01 18:58:23 +0200

    added MonthlyStatementViewModel class

---
+++ NoLooseCent/Models/MonthlyStatementViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace NoLooseCent.Models
+{
+    public class MonthlyStatementViewModel
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public string MonthName => new DateTime(Year, Month, 1).ToString("MMMM yyyy");
+
+        public string CurrencyCode { get; set; }
+
+        public decimal OpeningBalance { get; set; }
+        public decimal ClosingBalance { get; set; }
+
+        public IList<StatementEntry> Entries { get; set; } = new List<StatementEntry>();
+    }
+}
