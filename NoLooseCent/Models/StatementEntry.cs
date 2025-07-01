c52adf39b2595acbc4e633b5024c4ed2bd84ea62
Anold Kupara <kuparaanold20@gmail.com>
2025-07-01 18:55:57 +0200

    added StatementEntry class

---
+++ NoLooseCent/Models/StatementEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace NoLooseCent.Models
+{
+    public class StatementEntry
+    {
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public decimal? Deposit { get; set; }    // null if it is a withdrawal
+        public decimal? Withdrawal { get; set; } // null if it is a deposit
+        public decimal Balance { get; set; }     // running balance *after* the row
+    }
+}
